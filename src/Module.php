3c207bd7ff6b4de0a793779b7bcec0a14b913f30
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-08 20:48:02 +0100

    Moved files

---
+++ src/Module.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Log;
+
+use Phalcon\DiInterface;
+use VitesseCms\Admin\Utils\AdminUtil;
+use VitesseCms\Core\AbstractModule;
+
+class Module extends AbstractModule
+{
+    public function registerServices(DiInterface $di, string $string = null)
+    {
+        parent::registerServices($di, 'Log');
+        if (AdminUtil::isAdminPage()) :
+
+        else :
+
+        endif;
+    }
+}
