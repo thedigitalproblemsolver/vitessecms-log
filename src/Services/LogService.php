22bbbdf5ed492d53dc4de6502338ddda51dd9400
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-06-25 15:35:36 +0200

    Added message logging

--- src/Services/LogService.php
+++ src/Services/LogService.php
@@ -10,21 +10,28 @@ class LogService extends AbstractInjectable
 {
     public function write(ObjectId $itemId, string $class, string $message, bool $published = true): bool
     {
+        return $this->createLog($message, $published)->setItemId($itemId)->setClass($class)->save();
+    }
+
+    public function message(string $message, bool $published = true): bool
+    {
+        return $this->createLog($message, $published)->save();
+    }
+
+    private function createLog(string $message, bool $published = true): Log
+    {
         $userId = null;
         if($this->user !== null) :
             $userId = $this->user->getId();
         endif;
 
         return (new Log())
-            ->setItemId($itemId)
-            ->setClass($class)
-            ->setMessage($message)
             ->setUserId($userId)
             ->setPublished($published)
+            ->setMessage($message)
             ->setIpAddress($_SERVER['REMOTE_ADDR'])
             ->setProperty($_SERVER['HTTP_HOST'])
             ->setSourceUri($_SERVER['REQUEST_URI'])
-            ->setPost(serialize($this->request->getPost()))
-            ->save();
+            ->setPost(serialize($this->request->getPost()));
     }
 }
